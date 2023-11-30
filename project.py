c96c7279dc3310a3be85d3a742718643411b285c
a22009553 <a22102756@alunos.ulht.pt>
2023-11-30 18:01:12 +0000

    Commit 2 - problemas resolvidos: 1-> problema de numeros em falta em similar products 2-> problema dos primeios  5 digitos aparecerem como None na organização dos produtos semelhantes.
    Em falta mas a fazer: categories output; não aparecem todas as categorias
    
    Improvements made using a smaller dataset by debugging the code

--- project.py
+++ project.py
@@ -1,18 +1,83 @@
 dict = {}
+study_groups = {'Music', 'Book', 'DVD'}
 
-def ler_arquivo():
-    arquivo = "amazon-meta.txt"
-    f = open(arquivo, "r", encoding="UTF-8")
-    lines = f.readlines()
+def read_archive():
+    #archives = "teste.txt"
+
+    archives = "amazon-meta.txt"
+
+    file = open(archives, "r", encoding="UTF-8")
+    lines = file.readlines()
+    i = 0
+
+    current_asin = None
+    current_similarities = []
+    current_group = None
+    list_similarities = []
+    no_similarities = 0
+    categories_dict = {}
 
     for line in lines:
-        if "group: " in line.strip():
-            if line.split(":")[1].strip() in dict:
-                dict[line.split(":")[1].strip()] += 1
+        test = line.strip()
+        #searching for every type of object that exists in the dataset
+        if test.startswith("group: "):
+            #group exist in dictionary
+            if test.split(":")[1].strip() in dict:
+                dict[test.split(":")[1].strip()] += 1
+                current_group = test.split(":")[1].strip()
+
             else:
-                dict[line.split(":")[1].strip()] = 1
+                #group doesn't exist in dictionary
+                dict[test.split(":")[1].strip()] = 1
+                current_group = test.split(":")[1].strip()
 
-    f.close()
-    print(dict )
+        if test.startswith("similar: 0"):
+            no_similarities += 1
 
-ler_arquivo()
+        elif test.startswith("similar: "):
+            similarities = [sim[0:] for sim in test.split()[2:]]
+            current_similarities.extend(similarities)
+
+        # Check if the line is empty, indicating the end
+        elif not test:
+            # Add ASIN and its similarities to the list
+            list_similarities.extend([(current_asin, sim) for sim in current_similarities])
+
+        if test.startswith("ASIN: "):
+            current_asin = test.split(":")[1].strip()
+            current_similarities = []
+
+        if test.startswith("|") and (current_group in study_groups):
+            # Extracting the category and its number
+            ###print(current_group)
+            category_info = test.split("|")[-1].split("[")
+            category = category_info[0].strip()
+            category_number = category_info[1].split("]")[0].strip()
+
+            if category not in categories_dict:
+                categories_dict[category] = category_number
+
+    file.close()
+    print(dict)
+    print(no_similarities)
+
+    with open("similarities.txt", "w", encoding="UTF-8") as output_file:
+        for asin, similar_object in list_similarities:
+            output_file.write(f"{asin} - {similar_object}\n")
+
+    # Write the categories to a file
+    with open("categories_output.txt", "w", encoding="UTF-8") as output_file:
+        for category, id_category in categories_dict.items():
+            output_file.write(f"{category}: {id_category}\n")
+
+
+read_archive()
+
+
+# How many products are in the dataset? - DONE
+# How many products with no co-purchases? (meaning the product does not have “similar products”) - DONE
+# How many products per product type (book, movie or music)? - DONE
+# How many product categories? - DONE
+# How many products per category? -
+# How many users are making reviews? -
+# How many reviews per user? -
