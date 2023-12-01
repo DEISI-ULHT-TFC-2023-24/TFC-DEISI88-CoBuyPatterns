2167d9c23e71961943bf37f56c3a66f5e3152455
a22009553 <a22102756@alunos.ulht.pt>
2023-12-01 23:52:57 +0000

    Commit 4 - problemas resolvidos: 1-> Count das revies done (testes realizados com smaller dataset)
    NOTA: problemas tecnicos impedim-me de poder ter uma unica condição, pelo que sou obrigada a repeti-la
    
    Issue-> uma categoria nao tem nome. Maybe terá de ser cleaned.
    
    Falta:
    # How many products per category?

--- project.py
+++ project.py
@@ -1,10 +1,10 @@
 dict = {}
 study_groups = {'Music', 'Book', 'DVD'}
 
-def read_archive():
-    archives = "teste.txt"
 
-    #archives = "amazon-meta.txt"
+def read_archive():
+    #archives = "teste.txt"
+    archives = "amazon-meta.txt"
 
     file = open(archives, "r", encoding="UTF-8")
     lines = file.readlines()
@@ -16,49 +16,60 @@ def read_archive():
     list_similarities = []
     no_similarities = 0
     categories_dict = {}
+    customer_ids = {}
 
     for line in lines:
-        test = line.strip()
+        study_line = line.strip()
         #searching for every type of object that exists in the dataset
-        if test.startswith("group: "):
+        if study_line.startswith("group: "):
             #group exist in dictionary
-            if test.split(":")[1].strip() in dict:
-                dict[test.split(":")[1].strip()] += 1
-                current_group = test.split(":")[1].strip()
+            if study_line.split(":")[1].strip() in dict:
+                dict[study_line.split(":")[1].strip()] += 1
+                current_group = study_line.split(":")[1].strip()
 
             else:
                 #group doesn't exist in dictionary
-                dict[test.split(":")[1].strip()] = 1
-                current_group = test.split(":")[1].strip()
+                dict[study_line.split(":")[1].strip()] = 1
+                current_group = study_line.split(":")[1].strip()
 
-        if test.startswith("similar: 0"):
+        if study_line.startswith("similar: 0") and (current_group in study_groups):
             no_similarities += 1
 
-        elif test.startswith("similar: "):
-            similarities = [sim[0:] for sim in test.split()[2:]]
+        elif study_line.startswith("similar: ") and (current_group in study_groups):
+            similarities = [sim[0:] for sim in study_line.split()[2:]]
             current_similarities.extend(similarities)
 
         # Check if the line is empty, indicating the end
-        elif not test:
+        elif not study_line:
             # Add ASIN and its similarities to the list
             list_similarities.extend([(current_asin, sim) for sim in current_similarities])
 
-        if test.startswith("ASIN: "):
-            current_asin = test.split(":")[1].strip()
+        if study_line.startswith("ASIN: ") and (current_group in study_groups):
+            current_asin = study_line.split(":")[1]
             current_similarities = []
 
-        if test.startswith("|") and (current_group in study_groups):
+        if study_line.startswith("|") and (current_group in study_groups):
             # Extracting the category and its number
             ###print(current_group)
-            category_info = [category.strip() for category in test.split("|")[1:-1]]
+            category_info = [category.strip() for category in study_line.split("|")[1:-1]]
 
             for category in category_info:
                 if category not in categories_dict:
                     categories_dict[category] = ''
+            # ao ver uma categoria posso meter o numero à frente, ou seja A cat enters = 1, in another group i see her again, 2 and so on
+
+        if study_line.strip() and study_line[0].isdigit() and (current_group in study_groups):
+            customer_id = line.split()[2]
+            # Check if the customer ID is already in the array
+            if customer_id not in customer_ids:
+                customer_ids[customer_id] = 1
+            else:
+                customer_ids[customer_id] += 1
 
     file.close()
-    print(dict)
-    print(no_similarities)
+    print("How many products are in the dataset?", dict)
+    print("How many products with no co-purchases?", no_similarities)
+    print("How many users are making reviews?", len(customer_ids))
 
     with open("similarities.txt", "w", encoding="UTF-8") as output_file:
         for asin, similar_object in list_similarities:
@@ -69,6 +80,10 @@ def read_archive():
         for category, id_category in categories_dict.items():
             output_file.write(f"{category}: {id_category}\n")
 
+    with open("reviews.txt", "w", encoding="UTF-8") as output_file:
+        for review, qnt_review in customer_ids.items():
+            output_file.write(f"{review}: {qnt_review}\n")
+
 
 read_archive()
 
@@ -78,5 +93,5 @@ read_archive()
 # How many products per product type (book, movie or music)? - DONE
 # How many product categories? - DONE
 # How many products per category? -
-# How many users are making reviews? -
-# How many reviews per user? -
+# How many users are making reviews? - DONE
+# How many reviews per user? - DONE
