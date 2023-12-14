34d673ed60ab4fb4ea2d016aa6c58b0f5a701d71
a22009553 <a22102756@alunos.ulht.pt>
2023-12-14 13:46:17 +0000

    Commit 6- Nova formatação de dados. O objetivo é organizar os dados de forma a ficar tudo num só ficheiro
    .Certificar-me como fazer no caso das categorias e quantos reviews devo retirar e que informações devem estar listadas

--- project.py
+++ project.py
@@ -3,6 +3,8 @@ import csv
 dict = {}
 study_groups = {'Music', 'Book', 'DVD'}
 
+#para cada objeto quero: id, grupo, categorias, {'user_x' : comment}
+
 
 def read_archive():
     #archives = "teste.txt"
@@ -10,34 +12,27 @@ def read_archive():
 
     file = open(archives, "r", encoding="UTF-8")
     lines = file.readlines()
-    i = 0
 
+    organized_file = open('output.txt', 'w')
     current_asin = None
     current_similarities = []
     current_group = None
     list_similarities = []
     no_similarities = 0
-    categories_dict = {}
-    customer_ids = {}
+    category_info = []
+    customer_ids = []
+    category_list = []
+    unique_third_positions = set()
 
     for line in lines:
         study_line = line.strip()
         #searching for every type of object that exists in the dataset
         if study_line.startswith("group: "):
             current_group = study_line.split(":")[1].strip()
-            #group exist in dictionary
-            if study_line.split(":")[1].strip() in dict:
-                dict[study_line.split(":")[1].strip()] += 1
-            else:
-                #group doesn't exist in dictionary
-                dict[study_line.split(":")[1].strip()] = 1
 
         if study_line.startswith("ASIN: "):
             current_asin = study_line.split(":")[1]
             current_similarities = []
-
-        if study_line.startswith("similar: 0") and (current_group in study_groups):
-            no_similarities += 1
 
         elif study_line.startswith("similar: ") and (current_group in study_groups):
             similarities = [sim[0:] for sim in study_line.split()[2:]]
@@ -45,47 +40,34 @@ def read_archive():
             list_similarities.extend([(current_asin, sim) for sim in current_similarities])
 
         if study_line.startswith("|") and (current_group in study_groups):
-            # Extracting the category and its number
-            category_info = [line.strip() for line in study_line.split("\n") if line.startswith("|")]
-            #category_info = [category.strip() for category in study_line.split("|")[1:-1]]
-
-            for category in category_info:
-                if category not in categories_dict:
-                    categories_dict[category] = 1
-                else:
-                    categories_dict[category] += 1
+            lines = [line.strip() for line in study_line.split('\n') if line.startswith('|')]
+            result = []
+            for line in lines:
+                parts = line.split('|')[1:-1]
+                position = parts[2].split('[')[0].strip() if len(parts) > 2 else None
+                result.append(position)
+            # file -> ASIN - GROUP(CURRENT GROUP) - SIMIL - CATEGORY - COMENTARIOS
             # ao ver uma categoria posso meter o numero à frente, ou seja A cat enters = 1, in another group i see her again, 2 and so on
 
         if study_line.strip() and study_line[0].isdigit() and (current_group in study_groups):
-            customer_id = line.split()[2]
-            # Check if the customer ID is already in the array
-            if customer_id not in customer_ids:
-                customer_ids[customer_id] = 1
-            else:
-                customer_ids[customer_id] += 1
+            parts = line.split()
+            user_id = parts[2]
+            rating = parts[4]
+            if len(customer_ids) < 5:
+                customer_ids.append(f"{user_id} - {rating}")
+
+        if not study_line and (current_group in study_groups):
+            organized_file.write(f"{current_asin} , {current_group} , {current_similarities} , {result} , "
+                                 f"{customer_ids}\n")
+            customer_ids = []
 
     file.close()
-    print("How many products are in the dataset?", dict)
-    print("How many products with no co-purchases?", no_similarities)
-    print("How many users are making reviews?", len(customer_ids))
-
     # with open("similarities.txt", "w", encoding="UTF-8") as output_file:
     # for asin, similar_object in list_similarities:
     # output_file.write(f"{asin} - {similar_object}\n")
-
     # Escrever no arquivo CSV
-    with open("similarities.csv", "w", encoding="UTF-8") as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=':')
-        for asin, similar_object in list_similarities:
-            csv_writer.writerow([asin, similar_object])
-
-    with open("categories_output.txt", "w", encoding="UTF-8") as output_file:
-        for category, id_category in categories_dict.items():
-            output_file.write(f"{category}: {id_category}\n")
-
-    with open("reviews.txt", "w", encoding="UTF-8") as output_file:
-        for review, qnt_review in customer_ids.items():
-            output_file.write(f"{review}: {qnt_review}\n")
+    with open('output.txt', 'r') as organized_file:
+          conteudo = organized_file.read()
 
 
 read_archive()
