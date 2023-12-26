5b33afe8b4568557c6a0bd0e303536a5a96d9bb1
Joana Okica <a22102756@alunos.ulht.pt>
2023-12-26 14:23:29 +0000

    Commit nº 7: Já nao me recordo de como era a estrutura das categorias, por isso meti todas por ordem de frequencia.  Da mais frequente par a menos.
     o pandas falta: > % prods por tipo
    	> % de onde cada produto se enquadra nas categorias por exemplo existem x% de prods de terror

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import csv
+from collections import Counter
 
 dict = {}
 study_groups = {'Music', 'Book', 'DVD'}
@@ -13,16 +14,15 @@ def read_archive():
     file = open(archives, "r", encoding="UTF-8")
     lines = file.readlines()
 
-    organized_file = open('output.txt', 'w')
+    organized_file = 'output.csv'
     current_asin = None
     current_similarities = []
     current_group = None
     list_similarities = []
-    no_similarities = 0
-    category_info = []
+    current_title = []
+    result = {}
     customer_ids = []
-    category_list = []
-    unique_third_positions = set()
+    estrutura_final = []
 
     for line in lines:
         study_line = line.strip()
@@ -34,40 +34,61 @@ def read_archive():
             current_asin = study_line.split(":")[1]
             current_similarities = []
 
+        if study_line.strip().startswith("title"):
+            title = line.split(":", 1)[1].strip()
+            # Append the title to the array
+            current_title.append(title)
+
         elif study_line.startswith("similar: ") and (current_group in study_groups):
             similarities = [sim[0:] for sim in study_line.split()[2:]]
             current_similarities.extend(similarities)
             list_similarities.extend([(current_asin, sim) for sim in current_similarities])
 
-        if study_line.startswith("|") and (current_group in study_groups):
-            lines = [line.strip() for line in study_line.split('\n') if line.startswith('|')]
-            result = []
-            for line in lines:
-                parts = line.split('|')[1:-1]
-                position = parts[2].split('[')[0].strip() if len(parts) > 2 else None
-                result.append(position)
-            # file -> ASIN - GROUP(CURRENT GROUP) - SIMIL - CATEGORY - COMENTARIOS
-            # ao ver uma categoria posso meter o numero à frente, ou seja A cat enters = 1, in another group i see her again, 2 and so on
+        if study_line.strip().startswith("|") and (current_group in study_groups):
+            split_items = study_line.strip().split("|")[1:]
+
+            # Step 2: Exclude the first two elements from the split result, and split each remaining item by "["
+            split_items = split_items[2:]
+            parts = [item.split("[")[0] for item in split_items]
+            estrutura_final.append(parts)
+
+
+        #filtered_categories = {category: frequency for category, frequency in result.items() if frequency > 2}
+
+        # file -> ASIN - GROUP(CURRENT GROUP) - SIMIL - CATEGORY - COMENTARIOS
+        # ao ver uma categoria posso meter o numero à frente, ou seja A cat enters = 1, in another group i see her again, 2 and so on
 
         if study_line.strip() and study_line[0].isdigit() and (current_group in study_groups):
             parts = line.split()
             user_id = parts[2]
             rating = parts[4]
-            if len(customer_ids) < 5:
-                customer_ids.append(f"{user_id} - {rating}")
+            customer_ids.append(f"{user_id} - {rating}")
 
         if not study_line and (current_group in study_groups):
-            organized_file.write(f"{current_asin} , {current_group} , {current_similarities} , {result} , "
-                                 f"{customer_ids}\n")
+            palavras = [palavra for sublista in estrutura_final for palavra in sublista]
+            contagem_palavras = Counter(palavras)
+            palavras_mais_frequentes = [palavra for palavra, frequencia in contagem_palavras.most_common()]
+
+            organized_lines = [current_asin, current_group, current_title, current_similarities,
+                               palavras_mais_frequentes, customer_ids]
+
+            with open(organized_file, mode='a', newline='') as file:
+                csv_writer = csv.writer(file)
+                csv_writer.writerow(organized_lines)
+
             customer_ids = []
+            current_title = []
+            result = []
+            palavras = []
+            estrutura_final = []
 
     file.close()
     # with open("similarities.txt", "w", encoding="UTF-8") as output_file:
     # for asin, similar_object in list_similarities:
     # output_file.write(f"{asin} - {similar_object}\n")
-    # Escrever no arquivo CSV
-    with open('output.txt', 'r') as organized_file:
-          conteudo = organized_file.read()
+    with open('output.txt', mode='r') as file:
+        # Create a CSV reader object
+        csv_reader = csv.reader(file)
 
 
 read_archive()
@@ -77,6 +98,6 @@ read_archive()
 # How many products with no co-purchases? (meaning the product does not have “similar products”) - DONE
 # How many products per product type (book, movie or music)? - DONE
 # How many product categories? - DONE
-# How many products per category? -
+# How many products per category? - DONE
 # How many users are making reviews? - DONE
 # How many reviews per user? - DONE
