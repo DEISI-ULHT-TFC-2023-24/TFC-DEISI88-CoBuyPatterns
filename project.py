7505d78e74d2f7ac321d361028b86b6515ba686f
Joana Okica <a22102756@alunos.ulht.pt>
2024-01-06 19:21:14 +0000

    Commit nº 8: Correçao do erro da quantidade de produtos que o dataset tem. Uniao de codigo referente a criaçao de csv (ja nao é efetuda) e À parte do codigo correspondente À criaçao de um datagram com todas as informaçoes. > juntar o codigo refetente a product-product relation a este de limpeza de dataset (DONE IN GOOGLE COLAB, will add later)
     o pandas falta:
        > % prods por tipo
    	> % de onde cada produto se enquadra nas categorias por exemplo existem x% de prods de terror
    	> categorias - fica a mais frequente

--- project.py
+++ project.py
@@ -1,11 +1,14 @@
 import csv
 from collections import Counter
+import pandas as pd
 
 dict = {}
 study_groups = {'Music', 'Book', 'DVD'}
+dfs = []
+
 
 def read_archive():
-    #archives = "teste.txt"
+    # archives = "teste.txt"
     archives = "amazon-meta.txt"
 
     file = open(archives, "r", encoding="UTF-8")
@@ -20,11 +23,14 @@ def read_archive():
     result = {}
     customer_ids = []
     estrutura_final = []
-    count = 0
+    palavras_mais_frequentes = ""
 
     for line in lines:
         study_line = line.strip()
-        #searching for every type of object that exists in the dataset
+        # searching for every type of object that exists in the dataset
+        if study_line.startswith("group: "):
+            current_group = study_line.split(":")[1].strip()
+
         if study_line.startswith("ASIN: "):
             current_asin = study_line.split(":")[1]
             current_similarities = []
@@ -34,62 +40,71 @@ def read_archive():
             # Append the title to the array
             current_title.append(title)
 
-        if study_line.startswith("group: "):
-            current_group = study_line.split(":")[1].strip()
-
-        if study_line.startswith("similar: ") and (current_group in study_groups):
-            #right num
+        elif study_line.startswith("similar: ") and (current_group in study_groups):
             similarities = [sim[0:] for sim in study_line.split()[2:]]
             current_similarities.extend(similarities)
             list_similarities.extend([(current_asin, sim) for sim in current_similarities])
 
         if study_line.strip().startswith("|") and (current_group in study_groups):
             split_items = study_line.strip().split("|")[1:]
+
             # Step 2: Exclude the first two elements from the split result, and split each remaining item by "["
             split_items = split_items[2:]
-            parts1 = [item.split("[")[0] for item in split_items]
-            estrutura_final.append(parts1)
+            parts = [item.split("[")[0] for item in split_items]
+            estrutura_final.append(parts)
 
-        #reviews
         if study_line.strip() and study_line[0].isdigit() and (current_group in study_groups):
             parts = line.split()
             user_id = parts[2]
             rating = parts[4]
             customer_ids.append(f"{user_id} - {rating}")
 
-        if study_line.strip() == '' and (current_group in study_groups):
-            count += 1
+        if not study_line and (current_group in study_groups):
             palavras = [palavra for sublista in estrutura_final for palavra in sublista]
             contagem_palavras = Counter(palavras)
-            palavras_mais_frequentes = [palavra for palavra, frequencia in contagem_palavras.most_common()]
+            if contagem_palavras:
+                palavra_mais_frequente = contagem_palavras.most_common(1)[0][0]
+            else:
+                palavra_mais_frequente = ""
 
             organized_lines = [current_asin, current_group, current_title, current_similarities,
                                palavras_mais_frequentes, customer_ids]
 
-            with open(organized_file, mode='a', newline='') as file:
-                csv_writer = csv.writer(file)
-                csv_writer.writerow(organized_lines)
-
             customer_ids = []
             current_title = []
+            result = []
             palavras = []
             estrutura_final = []
             current_group = None
 
+            columns = ['id', 'tipo', 'titulo', 'categorias', 'reviews', 'similaridades']
+            # Iterar sobre cada linha
+
+            id = organized_lines[0]
+            tipo = organized_lines[1]
+            titulo = organized_lines[2]
+            similaridades = organized_lines[3]
+            categoria = organized_lines[4]
+            customer_ids = organized_lines[5]
+
+            # Convert lists to strings
+            titulo = str(titulo).strip("[]").replace("'", "")
+            similaridades = str(similaridades).strip("[]").replace("'", "")
+
+            data_dict = {
+                'id': id,
+                'tipo': tipo,
+                'titulo': titulo,
+                'categorias': categorias,
+                'reviews': customer_ids,
+                'similaridades': similaridades
+            }
+
+            dfs.append(data_dict)
+
     file.close()
-    print(count)
-    with open('output.txt', mode='r') as file:
-        csv.reader(file)
 
 
 read_archive()
-
-
-
-# How many products are in the dataset? - DONE
-# How many products with no co-purchases? (meaning the product does not have “similar products”) - DONE
-# How many products per product type (book, movie or music)? - DONE
-# How many product categories? - DONE
-# How many products per category? - DONE
-# How many users are making reviews? - DONE
-# How many reviews per user? - DONE
+x = pd.DataFrame(dfs)
+x
