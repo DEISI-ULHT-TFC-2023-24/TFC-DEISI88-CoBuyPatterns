24cc21fe49d60e6e3f9e575443c16c9f81fcd279
Joana Okica <a22102756@alunos.ulht.pt>
2024-01-19 20:25:56 +0000

    Commit nº 9: Entrega Intermedia de TFC
        > % prods por tipo - DONE
    	> % de onde cada produto se enquadra nas categorias por exemplo existem x% de prods de terror -DONE
    	> categorias - fica a mais frequente -DONE

--- project.py
+++ project.py
@@ -1,110 +1,117 @@
+# limpeza de dados e produçao do datagram
 import csv
 from collections import Counter
 import pandas as pd
 
-dict = {}
 study_groups = {'Music', 'Book', 'DVD'}
+dict = {}
 dfs = []
+dfs_cats = []
+dfs_similars = []
 
 
 def read_archive():
-    # archives = "teste.txt"
     archives = "amazon-meta.txt"
 
     file = open(archives, "r", encoding="UTF-8")
     lines = file.readlines()
-
-    organized_file = 'output.csv'
-    current_asin = None
-    current_similarities = []
-    current_group = None
+    asin = None
+    similarities = []
+    group = None
     list_similarities = []
-    current_title = []
-    result = {}
+    title = []
     customer_ids = []
-    estrutura_final = []
-    palavras_mais_frequentes = ""
+    cats_list = []
+    most_frequent_categories = ""
 
     for line in lines:
         study_line = line.strip()
-        # searching for every type of object that exists in the dataset
+        # obter o grupo
         if study_line.startswith("group: "):
-            current_group = study_line.split(":")[1].strip()
+            group = study_line.split(":")[1].strip()
 
+        # obter id do produto
         if study_line.startswith("ASIN: "):
-            current_asin = study_line.split(":")[1]
-            current_similarities = []
+            asin = study_line.split(":")[1]
+            similarities = []
 
+        # obter titulo do produto
         if study_line.strip().startswith("title"):
-            title = line.split(":", 1)[1].strip()
+            current_title = line.split(":", 1)[1].strip()
             # Append the title to the array
-            current_title.append(title)
+            title.append(current_title)
 
-        elif study_line.startswith("similar: ") and (current_group in study_groups):
+        # obter lista de produtos semelhantes
+        if study_line.startswith("similar: ") and (group in study_groups):
             similarities = [sim[0:] for sim in study_line.split()[2:]]
-            current_similarities.extend(similarities)
-            list_similarities.extend([(current_asin, sim) for sim in current_similarities])
+            similarities.extend(similarities)
+            list_similarities.extend([(asin, sim) for sim in similarities])
 
-        if study_line.strip().startswith("|") and (current_group in study_groups):
-            split_items = study_line.strip().split("|")[1:]
+        # obter categorias - 1º retiramos o espaço, 2º retiramos o '|', 3º retiramos os numero identificador da categoria
+        if study_line.strip().startswith("|") and (group in study_groups):
+            try:
+                # obtermos a categoria na posiçao 3 (colocamos 3 no array porque contamos com o espaço)
+                word_in_cats_list = study_line.strip().split('|')[3]
+                word_in_cats_list = word_in_cats_list.split("[")[0]
+            except:
+                word_in_cats_list = ""
 
-            # Step 2: Exclude the first two elements from the split result, and split each remaining item by "["
-            split_items = split_items[2:]
-            parts = [item.split("[")[0] for item in split_items]
-            estrutura_final.append(parts)
+            cats_list.append(word_in_cats_list)
 
-        if study_line.strip() and study_line[0].isdigit() and (current_group in study_groups):
+        # obter user id e rating de utilizadores que efetuaram avaliaçoes ao produto
+        if study_line.strip() and study_line[0].isdigit() and (group in study_groups):
             parts = line.split()
             user_id = parts[2]
             rating = parts[4]
             customer_ids.append(f"{user_id} - {rating}")
 
-        if not study_line and (current_group in study_groups):
-            palavras = [palavra for sublista in estrutura_final for palavra in sublista]
-            contagem_palavras = Counter(palavras)
-            if contagem_palavras:
-                palavra_mais_frequente = contagem_palavras.most_common(1)[0][0]
+        # quando chegamos ao fim da  informaçao de um dado produto, vamos agregar toda a informaçao ena lista-> organized_lines
+        if not study_line and (group in study_groups):
+            # começamos por verificar qual a catefgoria que é mais frequente
+            categories = [categorie for categorie in cats_list]
+            categories_count = Counter(categories)
+            if categories_count:
+                most_frequent_categories = categories_count.most_common(1)[0][0]
             else:
-                palavra_mais_frequente = ""
+                most_frequent_categories = ""
 
-            organized_lines = [current_asin, current_group, current_title, current_similarities,
-                               palavras_mais_frequentes, customer_ids]
+            # colocamos a informaçao do produto na lista
+            organized_lines = [asin, group, title, similarities, most_frequent_categories, customer_ids]
 
             customer_ids = []
-            current_title = []
-            result = []
-            palavras = []
-            estrutura_final = []
-            current_group = None
-
-            columns = ['id', 'tipo', 'titulo', 'categorias', 'reviews', 'similaridades']
-            # Iterar sobre cada linha
-
-            id = organized_lines[0]
-            tipo = organized_lines[1]
-            titulo = organized_lines[2]
-            similaridades = organized_lines[3]
-            categoria = organized_lines[4]
-            customer_ids = organized_lines[5]
-
-            # Convert lists to strings
-            titulo = str(titulo).strip("[]").replace("'", "")
-            similaridades = str(similaridades).strip("[]").replace("'", "")
+            title = []
+            categories = []
+            cats_list = []
+            group = None
 
             data_dict = {
-                'id': id,
-                'tipo': tipo,
-                'titulo': titulo,
-                'categorias': categorias,
-                'reviews': customer_ids,
-                'similaridades': similaridades
+                'id': organized_lines[0],
+                'group': organized_lines[1].strip("[]").replace("'", ""),
+                'title': organized_lines[2],
+                'similarities': organized_lines[3],
+                'categories': organized_lines[4],
+                'reviews': organized_lines[5]
+            }
+            dfs.append(data_dict)
+
+            data_dict = {
+                'id': organized_lines[0],
+                'categories': organized_lines[4]
+            }
+            dfs_cats.append(data_dict)
+
+            data_dict = {
+                'id': organized_lines[0],
+                'similarities': organized_lines[3]
             }
 
-            dfs.append(data_dict)
+            dfs_similars.append(data_dict)
 
     file.close()
 
 
 read_archive()
-x = pd.DataFrame(dfs)
-x
+full_dataSet_datagram = pd.DataFrame(dfs)
+catsDatagram = pd.DataFrame(dfs_cats)
+similarsDatagram = pd.DataFrame(dfs_similars)
+
